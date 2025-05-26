a41d1f64c0fd473caf2fa2db783a754713d3cdb1
leraroy <valeria98.val@gmail.com>
2025-05-27 00:18:12 +0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ const {platform, release, version} = require("node:os");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://api.clickup.com/api/v2",
     setupNodeEvents(on, config) {
       allureCypress(on, config, {
         environmentInfo: {
@@ -20,8 +19,6 @@ module.exports = defineConfig({
     env: {
       allureLogCypress:false,
       allureReuseAfterSpec: true,
-      token: 'pk_194691600_50Y62ZQB7XRCCZGOICZTU6YO0YWQAONK',
-      goalID: '90151207922'
     }
   },
 });
--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -10,28 +10,37 @@
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
+import * as allure from "allure-js-commons";
 
 describe('example to-do app', () => {
+
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
+    allure.step("Visit site", ()=>{
+      cy.visit('https://example.cypress.io/todo')
+    })
   })
 
   it('displays two todo items by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-    cy.get('.todo-list li').should('have.length', 2)
+    allure.step("Check default items", ()=>{
+      cy.get('.todo-list li').should('have.length', 2)
+    })
 
     // We can go even further and check that the default todos each contain
     // the correct text. We use the `first` and `last` functions
     // to get just the first and last matched elements individually,
     // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    allure.step("Check default items", ()=>{
+      cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+      cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    })
+
   })
 
   it('can add new todo items', () => {
@@ -44,7 +53,10 @@ describe('example to-do app', () => {
     // This input has a data-test attribute so we'll use that to select the
     // element in accordance with best practices:
     // https://on.cypress.io/selecting-elements
-    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+    allure.step("Add new item", ()=>{
+      cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+    })
+
 
     // Now that we've typed our new item, let's check that it actually was added to the list.
     // Since it's the newest item, it should exist as the last element in the list.
